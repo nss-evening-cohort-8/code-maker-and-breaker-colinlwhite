d69b9bb2b81fa2c8fe4a080988fc728a4f808cd7
Colin White <clwhite814@gmail.com>
2018-09-22 13:51:54 -0500

    WIP

--- main.js
+++ main.js
@@ -3,10 +3,28 @@
 const printToDom = (stringToPrint, whereToPrint) => {
     document.getElementById(whereToPrint).innerHTML += stringToPrint;
   }
+// TEST FUNCTION FOR PRINT TO DOM - START
+//   const test = () => {
+//       let test1 = 1 + 1; 
+//       printToDom(test1, 'outputOne');
+//   }
+//   test();
+// TEST FUNCTION FOR PRINT TO DOM - END
 
-  const test = () => {
-      let test1 = 1 + 1; 
-      printToDom(test1, 'outputOne');
-  }
+const buttonOne = document.getElementById('secretButton');
+const buttonTwo = document.getElementById('wordsButton');
+const inputOneElem = document.getElementById('inputOne');
+const outputOneElem = document.getElementById('outputOne');
+const inputTwoElem = document.getElementById('inputTwo');
+const outputTwoElem = document.getElementById('outputTwo');
 
-  test();
+
+
+
+
+buttonOne.addEventListener('click', (event) => {
+    console.log(event);
+});
+
+
+
