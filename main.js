c7bf8d365c8a6ee3a3e06c61f2ef5749a18d32d6
Colin White <clwhite814@gmail.com>
2018-09-22 13:13:20 -0500

    added a print to dom function and tested it for usability

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
+const printToDom = (stringToPrint, whereToPrint) => {
+    document.getElementById(whereToPrint).innerHTML += stringToPrint;
+  }
+
+  const test = () => {
+      let test1 = 1 + 1; 
+      printToDom(test1, 'outputOne');
+  }
+
+  test();
